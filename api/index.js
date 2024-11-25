2b99ec9ba207195aa6ba6a42d53c7e6fba605b81
ahadalireach <ahadali.reach@gmail.com>
2024-11-25 17:37:05 +0500

    Setup vercel.json, build configuration, and CORS paths for deployment

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://gher.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -25,6 +25,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/properties", propertyRoutes);
